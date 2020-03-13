1707a3097744b82bc001057c06d20d0fabe3f122
Igorth <igordiasth@gmail.com>
2020-03-13 17:16:40 +1300

    currenvy converter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Conversor from './components/Conversor/Conversor';
+import Header from './components/Header/index';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="linha">
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      <div className="linha">
+        <Conversor moedaA="NZD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="NZD"></Conversor>
+      </div>
     </div>
   );
 }
---
+++ src/components/Conversor/Conversor.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './Conversor.css';
+
+export default class Conversor extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      moedaA_valor: "",
+      moedaB_valor: 0
+    }
+
+    this.converter = this.converter.bind(this);
+  }
+
+  converter() {
+    
+    let to_for = `${this.props.moedaA}_${this.props.moedaB}`;
+    let url = `https://free.currconv.com/api/v7/convert?q=${to_for}&compact=y&apiKey=${apiKey}`;
+
+    fetch(url)
+      .then(res => {
+        return res.json()
+      })
+      .then(json => {
+        let cotacao = json[to_for].val;
+        let moedaB_valor = ( parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2)
+        this.setState({ moedaB_valor });
+      })
+  }
+  render() {
+    return (
+      <div className="conversor">
+        <h2>{this.props.moedaA} to {this.props.moedaB}</h2>
+        <input type="text" onChange={(event) => 
+          {
+              this.setState({moedaA_valor:event.target.value})
+          }
+        } name="" id=""/>
+        <input type="button" value="Converter" onClick={this.converter}/>
+        <h2>{this.state.moedaB_valor}</h2>
+      </div>
+
+    )
+  }
+}
